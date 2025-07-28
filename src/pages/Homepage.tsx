8fe593effddca75a4f8afea0668747235887ea86
Christina Tu <z5309205@ad.unsw.edu.au>
2025-07-28 20:36:07 +1000

    wip: main homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from '../css/Homepage.module.css';
 
 type HomepageProps = {
 
@@ -7,8 +8,17 @@ type HomepageProps = {
 export const Homepage: React.FC<HomepageProps> = ({}) => {
 
   return (
-    <>
-      Homepage
-    </>
+      <div id={styles.contentContainer} className={styles.centeredFlex}>
+        <div id={styles.eclipse} className={styles.centeredFlex}>
+          <div className={`${styles.textContainer} ${styles.centeredFlex}`}>
+            <h1>
+              <span>hi, </span>
+              <span className={styles.purpleText}>I'm Christina!</span>
+            </h1>
+            <p>Front-End Developer</p>
+            <div className={`${styles.buttonContainer} ${styles.centeredFlex}`}>Button</div>
+          </div>
+        </div>
+      </div>
   );
 };
