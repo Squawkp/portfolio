587185f8bbe72ac923c4bdf974f87de0e2726113
Christina Tu <z5309205@ad.unsw.edu.au>
2026-01-21 16:35:50 +1100

    feat: homepage styling and typing animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route, HashRouter} from "react-router-dom"
 import { Homepage } from './pages/Homepage'
 import { NoPage } from './pages/NoPage'
+import './App.css';
 
 const App: React.FC = () => {
 
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styles from '../css/Homepage.module.css';
 
 type HomepageProps = {
 
 }
 
+const HEADING_TEXT = `Hi I'm Christina!`;
+const FIRST_INDEX = 3;
+const LAST_INDEX = 7;
+
 export const Homepage: React.FC<HomepageProps> = ({}) => {
+  const [TypedText, setTypedText] = useState("");
+  const word1 = TypedText.slice(0, FIRST_INDEX);
+  const word2 = TypedText.slice(FIRST_INDEX, LAST_INDEX);
+  const word3 = TypedText.slice(LAST_INDEX, HEADING_TEXT.length);
+  
+  useEffect(() => {
+    // type one char every tick
+    const interval = setInterval(() => {
+      setTypedText(prev => {
+        const nextChar = HEADING_TEXT[prev.length];
+        if (!nextChar) {
+          clearInterval(interval);
+          return prev;
+        }
+        const newString = prev + nextChar;
+        return newString;
+      });
+    }, 150);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
-      Homepage
+      <div className={`${styles.pageBackground} ${styles.centreContent} ${styles.darkMode}`}>
+        <div className={`${styles.mainContentContainer} ${styles.centreContent}`}>
+          <h1 className={`${styles.TypingFont}`}>
+            <span>{word1}</span>
+            <span>{word2}</span>
+            <span className={`${styles.blinkingCursorAnimation} ${styles.colouredText}`}>{word3}</span>
+          </h1>
+          <h2>Frontend Developer</h2>
+          <h2>& UI/UX Designer</h2>
+        </div>
+      </div>
     </>
   );
 };
